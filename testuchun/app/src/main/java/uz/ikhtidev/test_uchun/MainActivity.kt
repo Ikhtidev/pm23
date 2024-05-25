ac5afdfaf11237ff38bf1a99815c04395cb4ac9d
Ikhtidev <ixtiyoryarashov14011401@gmail.com>
2024-05-25 12:48:53 +0500

    loyiha yuklandi

---
+++ testuchun/app/src/main/java/uz/ikhtidev/test_uchun/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.ikhtidev.test_uchun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
